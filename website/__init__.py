cfcc0297c0c8e400afd760110c88fed5aa274fb3
dima999572 <dima_dubinevich@gmail.com>
2023-05-30 20:56:51 +0200

    Added Log In, Sign In, and Logout pages.
    Added a Blueprint responsible for auth.py, where you can find /login, /logout, and /sign-up routes. Also added the auth_base template for extending templates such as login, logout, and sign-up. Additionally, a navbar was created on all pages, providing the opportunity to redirect to the login, logout, sign-up, and home pages.

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,9 @@ def create_app():
     app.config["SECRET_KEY"] = os.environ.get("AUDIOWAVE_SECRET_KEY")
 
     from website.views.public.start import start_bp
+    from website.views.public.auth import auth_bp
 
     app.register_blueprint(start_bp, url_prefix='/')
+    app.register_blueprint(auth_bp, url_prefix='/')
 
     return app
