8923a99426a61a3665970318eac7acad7e467488
dima999572 <102175283+dima999572@users.noreply.github.com>
2023-06-11 00:37:48 +0200

    Add Home Page with YouTube Video Audio Extraction (#11)
    - Add audios folder to .gitignore \n- Add install.py script for installing and converting audio 
    - Add home_bp Buepring 
    - Implement audio installation and handling Audio Database Model in home.py
    - Create home.html template with audio input form

--- website/__init__.py
+++ website/__init__.py
@@ -17,9 +17,11 @@ def create_app():
 
     from website.views.public.start import start_bp
     from website.views.public.auth import auth_bp
+    from website.views.public.home import home_bp
 
     app.register_blueprint(start_bp, url_prefix='/')
     app.register_blueprint(auth_bp, url_prefix='/')
+    app.register_blueprint(home_bp, url_prefix='/')
 
     if not path.exists('instance/' + DB_NAME):
         with app.app_context():
